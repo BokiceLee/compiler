30908bd5ced5bbb37dc604c7b8407101f6cf758f
BokiceLee <bokice@qq.com>
2016-11-09 23:31:52 +0800

    add head and modify about ident or IDENT

--- Compiler4C0/main.c
+++ Compiler4C0/main.c
@@ -2,77 +2,7 @@
 #include <stdlib.h>
 #include<string.h>
 #include<ctype.h>
-#define MAX_LEN_OF_FILE 1024
-#define MAX_LEN_OF_LINE 1024
-#define MAX_LEN_OF_TOKEN 10
-#define LEN_OF_STRING_TABLE 200
-#define LEN_OF_STRING 200
-#define MAX_LEN_OF_NUM 8
-#define MAX_NUM 99999999
-#define REVERSED_NUM 16
-#define SPECIAL_SYM_NUM 20
-#define ERROR_NUM 9
-enum symbol{
-    beginsy=0,casesy,charsy,constsy,defaultsy,elsesy,endsy,
-    ifsy,intsy,mainsy,printfsy,returnsy,scanfsy,
-    switchsy,voidsy,whilesy,
-    eql,neq,gtr,geq,lss,leq,
-    plus,minus,times,idiv,
-    lparent,rparent,lbrack,rbrack,lfbrace,rfbrace,
-    comma,semicolon,colon,becomes,//colon:冒号,semicolon:分号
-    ident,intcon,charcon,stringcon,illegalcon
-};
-char* special_symbol[SPECIAL_SYM_NUM]={
-    "eql",//0
-    "neq",//1
-    "gtr",//2
-    "geq",//3
-    "lss",//4
-    "leq",//5
-    "plus",//6
-    "mius",//7
-    "times",//8
-    "idiv",//9
-    "lparent",//10
-    "rparent",//11
-    "lbrack",//12
-    "rbrack",//13
-    "lfbrace",//14
-    "rfbrace",//15
-    "comma",//16
-    "semicolon",//17
-    "colon",//18
-    "becomes"//19
-};
-char* reversed_table[REVERSED_NUM]={
-    "begin",//0
-    "case",//1
-    "char",//2
-    "const",//3
-    "default",//4
-    "else",//5
-    "end",//6
-    "if",//7
-    "int",//8
-    "main",//9
-    "printf",//10
-    "return",//11
-    "scanf",//12
-    "switch",//13
-    "void",//14
-    "while"//15
-};
-char* errormsg[ERROR_NUM]={
-    "num begin with zero",
-    "number should not end with letter or identity should not begin with number,maybe missing a space?",
-    "error too big num",
-    "wrong char",
-    "error of char",
-    "wrong char in string",
-    "error '!'",
-    "illegal character",
-    "incomplete"
-};
+#include "MyHead.h"
 int errors=0;
 FILE *fsource;//源代码文件指针
 FILE *ftarget;//目标代码输出文件指针
@@ -92,6 +22,7 @@ int string_index=0;
 int i_temp;
 int output2where_console_0_file_1=0;
 int optimization=0;
+int return_temp=0;
 void error(int error_code){
     switch(error_code){
     case 0:
@@ -235,10 +166,14 @@ void getNextCh(){
         error(8);
         exit(0);
     }
-    ch=tolower(ch);
-    if(ch=='\n'){
-        column_in_source_file=0;
+    if(return_temp==1){
+        return_temp=0;
         row_in_source_file++;
+        column_in_source_file=0;
+    }
+    if(ch=='\n'){
+        return_temp=1;
+        column_in_source_file++;
     }else if(ch=='\t'){
         column_in_source_file+=4;
     }else{
@@ -331,18 +266,26 @@ void getNextSym(){
             getNextCh();
             break;
         case '{':
-            sym=lfbrace;
+            sym=lquote;
             getNextCh();
             break;
         case '}':
-            sym=rfbrace;
+            sym=rquote;
             getNextCh();
             break;
         case '\''://??????????
             sym=charcon;
             getNextCh();
             if(!(is_add_op_char(ch)||is_mul_op_char(ch)||is_digital(ch)||is_letter(ch))){
-                error(3);
+                if(ch=='\''){
+                    error(9);
+                    token[0]='\0';
+                    read_ch_len++;
+                    getNextCh();
+                    break;
+                }else{
+                    error(3);
+                }
             }
             token[0]=ch;
             do{
